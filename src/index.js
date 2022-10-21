4823524fe2a627b3c7cebf9212dd862342b20d3c
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-21 23:56:44 +0600

    Writing up the Provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import { configureStore } from "redux";
 
-ReactDOM.render(<App></App>, document.querySelector("#root"));
+import App from "./components/App";
+import reducers from "./reducers";
+
+ReactDOM.render(
+  <Provider store={configureStore(reducers)}>
+    <App></App>
+  </Provider>,
+  document.querySelector("#root")
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 export const songReducer = () => {
   return [
     {
@@ -26,3 +28,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  song: songReducer,
+  selectedSong: selectedSongReducer,
+});
