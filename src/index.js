e174b9c95d995da5152a92d1c92dabc796e3581b
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-22 00:08:28 +0600

    The Connect Function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { configureStore } from "redux";
+import { createStore } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
 ReactDOM.render(
-  <Provider store={configureStore(reducers)}>
+  <Provider store={createStore(reducers)}>
     <App></App>
   </Provider>,
   document.querySelector("#root")
