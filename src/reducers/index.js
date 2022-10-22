8e7e0f674c78e65d8fe6c15c1e64303efbfd5a5a
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-22 12:40:50 +0600

    Configuring Connect with MapStateToProps

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,11 @@
 import { combineReducers } from "redux";
 
-export const songReducer = () => {
+const songsReducer = () => {
   return [
-    {
-      title: "No Scrubs",
-      duration: "4:05",
-    },
-    {
-      title: "Macarena",
-      duration: "2:30",
-    },
-    {
-      title: "All Star",
-      duration: "3:15",
-    },
-    {
-      title: "I Want it That Way",
-      duration: "1:45",
-    },
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "All Star", duration: "3:15" },
+    { title: "I Want it That Way", duration: "1:45" },
   ];
 };
 
@@ -30,6 +18,6 @@ const selectedSongReducer = (selectedSong = null, action) => {
 };
 
 export default combineReducers({
-  song: songReducer,
+  songs: songsReducer,
   selectedSong: selectedSongReducer,
 });
