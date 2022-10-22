f777840ec2d124d15157ac7f7c5b678681b9d8db
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-22 13:24:53 +0600

    Functional Components with Connect

---
+++ src/components/SongDetails.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongDetails = (props) => {
+  console.log(props);
+  return <div>SongDetails</div>;
+};
+
+const mapStateToProps = (state) => {
+  return {
+    song: state.selectedSong,
+  };
+};
+
+export default connect(mapStateToProps)(SongDetails);
